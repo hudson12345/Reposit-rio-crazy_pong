90b7aba519b1bb052bacd5fa83bde3320a4b3dd0
hudson12345 <hud.a.barreiro@gmail.com>
2024-04-22 12:12:51 -0300

    atualização

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-let canvasPong = document.getElementById('canvas')/*varial para manipular o elemento canvas atraves do id*/
-let quadro = canvasPong.getContext('2d')/*deixa o canvas pronto para receber desenhos 2D*/
-
-let player1 = quadro.fillRect(100,100,50,50)
+let canvasPong = document.getElementById('canvas');/*varial para manipular o elemento canvas atraves do id*/
+let quadro = canvasPong.getContext('2d');/*deixa o canvas pronto para receber desenhos 2D*/
+quadro.fillStyle = "red";
+quadro.fillRect(100, 100, 200, 200);
